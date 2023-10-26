353845d99638d250cd3181183e32815fabc28ac3
camcasuga <carlislemarquez.casuga@gmail.com>
2023-10-26 10:20:41 +0300

    commit from desktop check for any merge conflicts

--- my_plotting.py
+++ my_plotting.py
@@ -193,7 +193,10 @@ def plot_diagonal(pred, true):
     diag = np.linspace(0.0, np.max(pred) + 0.3, 100)
     ax.plot(diag, diag, color = 'black', linestyle = '--', alpha = 0.5)
 
-    for i in range(403):
+    # generate random 100 random i from 0 to 403 and put into a list
+    Is = [np.random.randint(0, 403) for j in range(100)]
+
+    for i in Is:
         ax.plot(true[:,i], pred[:,i], '.', color = 'g', alpha = 0.7)
 
     ax.set_xlabel("Model $\sigma_r$")
@@ -235,9 +238,9 @@ def plot_zscore(pred, true, sd, bins_ = 30, text_x = 0.05, text_y = 0.95):
     ax.hist(z.flatten(), bins = bins_, density = True, color = 'g', alpha = 0.7, label = "Emulator")
     ax.plot(x_fit, gauss_fit, color = 'g', alpha = 0.7, linewidth = 2, linestyle = '--')
     ax.plot(x, gauss, color = 'black', linewidth = 2, linestyle = '--', label = "Target")
-    ax.text(text_x, text_y, "Mean = {:.3f}\nSd = {:.3f}".format(np.mean(z.flatten()), np.std(z.flatten())), transform=ax.transAxes, fontsize = 16)
+    ax.text(text_x, text_y, "Mean = {:.3f}\nSd = {:.3f}".format(np.mean(z.flatten()), np.std(z.flatten())), transform=ax.transAxes, fontsize = 20)
     ax.set_xlabel("z-score")
-    ax.legend()
+    ax.legend(fontsize = 20)
     return fig, ax
 
 def display_median(paramsamples, param_names):
@@ -322,9 +325,9 @@ def plot_posterior_mean_and_ub(q2s, ss, model_values, exp_df, exp_err, title_ = 
     return fig, ax
 
 
-def plot_corner(mve_samples, mv5_samples):
+def plot_corner(mve_samples, mv5_samples, color_mv5 = 'b', color_mve = 'r'):
     hm = [0.06, 18.9, 7.2, 16.36, 1.0]
-    param_names = [r"$Q_{s0}^{2}$ [GeV²]",
+    param_names = [r"$Q_{s,0}^{2}$ [GeV²]",
                r"$e_c$",
                r"$C^{2}$",
                r"$\sigma_0/2$ [mb]",
@@ -361,7 +364,7 @@ def plot_corner(mve_samples, mv5_samples):
                    'bins': 30}
     hist2d_kwargs = {"bins" : 30, 
                      "smooth" : 1.7,
-                     "plot_datapoints" : True,
+                     "plot_datapoints" : False,
                      "plot_density":True,}
     
     for i in range(5):
@@ -371,33 +374,33 @@ def plot_corner(mve_samples, mv5_samples):
             n_mv5 = gaussian_filter(n_mv5, sigma = 1.5)
             x0 = np.array(list(zip(bins_mv5[:-1], bins_mv5[1:]))).flatten()
             y0 = np.array(list(zip(n_mv5, n_mv5))).flatten()
-            axes[i,i].plot(x0, y0, color = 'b', **hist_plot_kwargs)
+            axes[i,i].plot(x0, y0, color = color_mv5, **hist_plot_kwargs)
             
             axes[i,i].set_xlim(xranges[i])
             axes[i,i].set_ylim([0.0,None])
-            axes[i,i].axvline(1.0, color = 'r', linestyle = '-', linewidth = 3.0)
+            axes[i,i].axvline(1.0, color = color_mve, linestyle = '-', linewidth = 3.0)
 
         else:
             n_mv5, bins_mv5 = np.histogram(mv5_samples[:,i], **hist_kwargs)
             n_mv5 = gaussian_filter(n_mv5, sigma = 1.5)
             x0 = np.array(list(zip(bins_mv5[:-1], bins_mv5[1:]))).flatten()
             y0 = np.array(list(zip(n_mv5, n_mv5))).flatten()
-            axes[i,i].plot(x0, y0, color = 'b', **hist_plot_kwargs)
+            axes[i,i].plot(x0, y0, color = color_mv5, **hist_plot_kwargs)
             
             n_mve, bins_mve = np.histogram(mve_samples[:,i],**hist_kwargs)
             n_mve = gaussian_filter(n_mve, sigma = 1.5)
             x02 = np.array(list(zip(bins_mve[:-1], bins_mve[1:]))).flatten()
             y02 = np.array(list(zip(n_mve, n_mve))).flatten()
-            axes[i,i].plot(x02, y02, color = 'r', **hist_plot_kwargs)
+            axes[i,i].plot(x02, y02, color = color_mve, **hist_plot_kwargs)
             axes[i,i].set_xlim(xranges[i])
             axes[i,i].set_ylim([0.0 , None])
 
         for j in range(i):
-            corner.hist2d(mve_samples[:,i], mve_samples[:,j], ax = axes[j,i], color = 'r', **hist2d_kwargs, contour_kwargs = {'linewidths':2.0})
+            corner.hist2d(mve_samples[:,i], mve_samples[:,j], ax = axes[j,i], color = color_mve, **hist2d_kwargs, contour_kwargs = {'linewidths':2.0})
             axes[j,i].set_xlim(xranges[i])
             axes[j,i].set_ylim(xranges[j])
             axes[j,i].tick_params(which='major', labelrotation=35)
-            corner.hist2d(mv5_samples[:,j], mv5_samples[:,i], ax = axes[i,j], color = 'b', **hist2d_kwargs, contour_kwargs = {'linewidths':2.0})
+            corner.hist2d(mv5_samples[:,j], mv5_samples[:,i], ax = axes[i,j], color = color_mv5, **hist2d_kwargs, contour_kwargs = {'linewidths':2.0})
             axes[i,j].set_xlim(xranges[j])
             axes[i,j].set_ylim(xranges[i])
             axes[i,j].tick_params(which='major', labelrotation=35)
@@ -418,8 +421,9 @@ def plot_corner(mve_samples, mv5_samples):
     axes[0,0].tick_params(which='major', axis = 'y', size = 0)
 
     for i in range(1,5):
-        axes[i,0].set_ylabel(param_names[i], fontsize = 24)
-        axes[i-1,3].set_ylabel(param_names[i-1], fontsize = 24)
+        axes[i,0].set_ylabel(param_names[i], fontsize = 24) #if ((i != 0) & (i != 4)) else axes[i,0].set_ylabel(param_names[i], fontsize = 24, labelpad = 10)
+        axes[i-1,3].set_ylabel(param_names[i-1], fontsize = 24) #if i != 1 else axes[i-1,3].set_ylabel(param_names[i-1], fontsize = 24, labelpad = 10)
+
         #axes[i-1,3].set_yticklabels([])
         axes[i-1,3].yaxis.set_label_position("right")
         axes[i-1,3].yaxis.tick_right()
@@ -429,8 +433,8 @@ def plot_corner(mve_samples, mv5_samples):
     
     # manually removing and arranging tick positions and axis labels
     for i in range(5):
-        axes[4,i].set_xlabel(param_names[i], fontsize = 24)
-        axes[0,i].set_xlabel(param_names[i], fontsize = 24)
+        axes[4,i].set_xlabel(param_names[i], fontsize = 24) #if ((i != 0) & (i != 4)) else axes[4,i].set_xlabel(param_names[i], fontsize = 24, labelpad = 10)
+        axes[0,i].set_xlabel(param_names[i], fontsize = 24) #if ((i != 0) & (i != 4)) else axes[0,i].set_xlabel(param_names[i], fontsize = 24, labelpad = 10)
         axes[0,i].xaxis.set_label_position("top")
         axes[0,i].xaxis.tick_top()
         for j in range(1,4):
@@ -440,37 +444,5 @@ def plot_corner(mve_samples, mv5_samples):
     axes[3,3].set_ylabel("")
     axes[3,3].set_yticklabels([])
     axes[3,3].tick_params(which='major', axis = 'y', size = 0)
+    fig.align_labels()
     return fig, axes
-
-def plot_pred_mve_vs_mv5(mve_values, mv5_values, x, ylabel, xlabel, title_ = "", legend_loc = "lower right", xlogscale = False, ylogscale = False, linewidth_ = 2):
-    ''' 
-    Plot initial dipole shape or 2DFT 
-    Input: mve_values, a list of mean, upper sd, and lower sd values for the mve model
-           mv5_values, a list of mean, upper sd, and lower sd values for the mv5 model    
-    '''
-    mve_usd = mve_values[0] + 2*mve_values[1]
-    mve_dsd = mve_values[0] - 2*mve_values[2]
-    mv5_usd = mv5_values[0] + 2*mv5_values[1]
-    mv5_dsd = mv5_values[0] - 2*mv5_values[2]
-
-    fig, ax = plt.subplots(1,1, figsize = (8,6))
-    ax.plot(x, mv5_values[0], '--',linewidth = linewidth_, color = "b")
-    ax.fill_between(x, mv5_usd, mv5_dsd, alpha = 0.6, color = "b")
-    ax.plot(x, mve_values[0], '--',linewidth = linewidth_, color = "r")
-    ax.fill_between(x, mve_usd, mve_dsd, alpha = 0.6, color = "r")
-
-    blue_line = Line2D([0], [0], color='b', linestyle='--', linewidth=2)
-    blue_patch = mpatches.Patch(color='b', alpha = 0.6)
-    red_line = Line2D([0], [0], color='r', linestyle='--', linewidth=2)
-    red_patch = mpatches.Patch(color='r', alpha = 0.6)
-    handles_ = [(blue_patch, blue_line),(red_patch, red_line), ]
-    ax.legend(handles= handles_, 
-           labels = ["[$ Q_{s0}^{2}, e_{c}, C^{2}, \sigma_{0}/2, \gamma$]", "[$ Q_{s0}^{2}, e_{c}, C^{2}, \sigma_{0}/2$]"], 
-           loc = legend_loc)
-    ax.set_title(title_)
-    ax.set_xscale("log") if xlogscale else None
-    ax.set_yscale("log") if ylogscale else None
-    ax.set_ylabel(ylabel)
-    ax.set_xlabel(xlabel)
-
-    return fig, ax
